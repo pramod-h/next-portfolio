d9831535a6c94d95d0877f4e4fc333936c5174fd
Pramod <pramod.h@codilar.com>
2025-02-06 22:33:44 +0530

    Added vercel analytics

--- components/UI/Layout/Layout.jsx
+++ components/UI/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import styles from './Layout.module.scss';
 export default function Container({children, className=''}) {
@@ -6,6 +7,7 @@ export default function Container({children, className=''}) {
     return (
         <div className={`${styles.container} ${className}`}>
             {children}
+            <SpeedInsights />
         </div>
     );
 }
